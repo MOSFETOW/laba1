cc996df9d3184986bc21e45881bd1f0796a8c588
Саша <150384647+MOSFETOW@users.noreply.github.com>
2025-09-04 01:27:57 +0300

    Add files via upload

---
+++ лаба.py
@@ -0,0 +1,45 @@
+age=16
+height=156.5
+text="Hellow"
+fruitList=['apple','orange','lemon']
+colorOfCloth={"jaket":"black","trousers":"blue"}
+randomNumbers={3,243,5,6,4,6,7}
+isOld=False
+
+print("age, int:",age,"height, float:",height,"text,  string:",text,"fruitList,list:",
+      fruitList,"colorOfCloth,dict:",colorOfCloth,
+"randomNumbers, set:",randomNumbers,"isOld, bool:",isOld)
+
+exampleInt=726
+
+exampleInt+=6
+
+print(exampleInt)
+
+
+
+exampleInt-=6
+
+print(exampleInt)
+
+
+exampleInt*=6
+
+print(exampleInt)
+exampleInt/=3
+
+print(exampleInt)
+
+exampleInt//=2
+
+print(exampleInt)
+
+exampleInt**=2
+
+print(exampleInt)
+
+exampleInt%=56
+
+print(exampleInt)
+
+
